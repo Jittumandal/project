4c426f389532f957d4177b9c6f5dd2a17b6cfc59
[jitender] <jitendrakumar.designer@gmail.com>
2022-02-28 13:01:07 +0530

    jittu

--- src/BootSnav.jsx
+++ src/BootSnav.jsx
@@ -16,7 +16,7 @@ const BootSnav = () =>{
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action1">Home1</Nav.Link>
         <Nav.Link href="#action2">Link</Nav.Link>
         <NavDropdown title="Link" id="navbarScrollingDropdown">
           <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
